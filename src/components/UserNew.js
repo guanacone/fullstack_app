a2edd38c8b1435e9ef617aec7fb594fde956d6b7
Gilles Rusca <gilles.rusca@gmail.com>
2020-09-26 08:44:25 -0600

    initial setup

---
+++ src/components/UserNew.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const UserNew = () => (<h3>New User Form</h3>);
+
+export default UserNew;
--- src/pages/user.js
+++ src/pages/user.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router } from '@reach/router';
 import UserIndex from '../components/UserIndex';
 import UserProfile from '../components/UserProfile';
+import UserNew from '../components/UserNew';
 
 const User = () => (
   <Router>
     <UserIndex path='/user' />
     <UserProfile path='/user/:id' />
+    <UserNew path='/user/new' />
   </Router>
 );
 
