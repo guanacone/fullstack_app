6247ac3698458ac7bc85cb4537d96fe1dcb57153
Gilles Rusca <gilles.rusca@gmail.com>
2020-09-26 14:58:00 -0600

    new user form

--- src/components/UserNew.js
+++ src/components/UserNew.js
@@ -1,5 +1,67 @@
 import React from 'react';
+import axios from 'axios';
+import { navigate } from 'gatsby';
+import url from '../url';
 
-const UserNew = () => (<h3>New User Form</h3>);
+export default class IndexPage extends React.Component {
+  state = {
+    firstName: '',
+    familyName: '',
+  }
 
-export default UserNew;
+  handleInputChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post(`${url}/api/user`, {
+      firstName: `${this.state.firstName}`,
+      familyName: `${this.state.familyName}`,
+    })
+      .then((response) => {
+        const id = response.data._id;
+        navigate(`/user/${id}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <form
+      name="contact"
+      method="post"
+      action="/thanks/"
+      onSubmit={this.handleSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
+        <label>
+          First name
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Family name
+          <input
+            type="text"
+            name="familyName"
+            value={this.state.familyName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
