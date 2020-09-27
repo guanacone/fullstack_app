e71d7aeeb449103af1d14068aa195e2fa8eccc5a
Gilles Rusca <gilles.rusca@gmail.com>
2020-09-27 08:59:18 -0600

    renamed UserNew to UserForm

--- src/pages/user.js
+++ src/pages/user.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Router } from '@reach/router';
 import UserIndex from '../components/UserIndex';
 import UserProfile from '../components/UserProfile';
-import UserNew from '../components/UserNew';
+import UserForm from '../components/UserForm';
 
 const User = () => (
   <Router>
     <UserIndex path='/user' />
     <UserProfile path='/user/:id' />
-    <UserNew path='/user/new' />
+    <UserForm path='/user/new' />
   </Router>
 );
 
