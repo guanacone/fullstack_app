40ad87ea611e777d23c998150aadcaee941d2834
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-04 08:28:37 -0600

    removed user destroy path

--- src/pages/user.js
+++ src/pages/user.js
@@ -4,7 +4,6 @@ import UserIndex from '../components/UserIndex';
 import UserProfile from '../components/UserProfile';
 import UserNew from '../components/UserNew';
 import UserEdit from '../components/UserEdit';
-import UserDestroy from '../components/UserDestroy';
 
 const User = () => (
   <Router>
@@ -12,7 +11,6 @@ const User = () => (
     <UserProfile path='/user/:id' />
     <UserNew path='/user/new' />
     <UserEdit path='/user/:id/edit' />
-    <UserDestroy path='/user/:id/delete'/>
   </Router>
 );
 
