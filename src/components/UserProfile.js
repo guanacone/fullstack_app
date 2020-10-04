a776fce590fe10102829ea4337fb6b2ea48089af
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-04 08:24:34 -0600

    modified user destroy implementaion

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,7 +1,44 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
+import axios from 'axios';
+import styled from 'styled-components';
 import useAPI from '../hooks/useAPI';
 import url from '../url';
+
+const DeleteButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  color: #007bff;
+  cursor: pointer;
+
+  &:hover{
+  color: #0056b3;
+  text-decoration: underline;
+  }
+
+  &:focus{
+    outline: none;
+  }
+`;
+
+const submitToApi = async (endpoint) => {
+  try {
+    const response = await axios.delete(
+      endpoint,
+    );
+    console.log(response);
+    navigate('/user');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteUser = (endpoint) => {
+  if (window.confirm('Do you want to delete the user?')) {
+    submitToApi(endpoint);
+  }
+};
 
 const User = ({ id }) => {
   const data = useAPI({ url: `${url}/api/user/${id}` });
@@ -16,11 +53,10 @@ const User = ({ id }) => {
       >
         Edit
       </Link>
-      <Link
-        to={`/user/${data._id}/delete`}
-      >
-        Delete
-      </Link>
+      <DeleteButton
+        type="button"
+        onClick={() => deleteUser(`${url}/api/user/${id}`)}>Delete User</DeleteButton>
+
     </div>
     )
   );
