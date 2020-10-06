3fd76eba34b7de7696030c576b0b9aca149285cd
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-06 09:44:00 -0600

    added error handling

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -2,16 +2,19 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useAPI = ({ url }) => {
-  const [content, setcontent] = useState(null);
+  const [content, setContent] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const result = await axios.get(url);
-      setcontent(result.data);
+      try {
+        const result = await axios.get(url);
+        setContent(result.data);
+      } catch (err) {
+        setContent(err);
+      }
     })();
   }, []);
-
-  return content;
+  return { content };
 };
 
 export default useAPI;
