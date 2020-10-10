1245fc24b3309b784b2cf042af2b8eeff4db9092
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-10 07:27:09 -0600

    set axios base  url

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
+import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import url from './src/utils/url';
+
+const onInitialClientRender = () => {
+  Axios.defaults.baseURL = `${url}`;
+};
+
+onInitialClientRender();
