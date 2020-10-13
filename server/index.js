54685c5841bc6cfed8beefd5d9a28379a836724c
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-13 15:00:03 -0600

    user route handlers

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const gatsby = require('gatsby-plugin-nodejs');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userController = require('./controllers/userController');
+// const userController = require('./controllers/userController');
 
 const app = express();
 
@@ -10,6 +10,7 @@ const mongoDB = 'mongodb+srv://mogador:basket@cluster0.twty6.mongodb.net/fullsta
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const user = require('./routes/user');
 
 app.use(cors());
 app.use(express.json());
@@ -18,11 +19,7 @@ gatsby.prepare({ app }, () => {
   app.get('/api', (req, res) => {
     res.json({ msg: 'Fetching data from API...' });
   });
-  app.get('/api/user', userController.indexUser);
-  app.post('/api/user', userController.createUser);
-  app.get('/api/user/:id', userController.showUser);
-  app.put('/api/user/:id', userController.updateUser);
-  app.delete('/api/user/:id', userController.destroyUser);
+  app.use('/api/user', user);
   // eslint-disable-next-line no-unused-vars
   app.use((err, req, res, _next) => {
     return res
