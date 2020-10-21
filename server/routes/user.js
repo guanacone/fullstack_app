9c35fa3bcf4562e8b02ea233fa446325416cea33
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-21 15:36:01 -0600

    protected additional routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const passport = require('passport');
 
-router.get('/', userController.indexUser);
+router.get('/', passport.authenticate('jwt', { session: false }), userController.indexUser);
 router.post('/', userController.createUser);
-router.get('/:id', userController.showUser);
-router.put('/:id', passport.authenticate('jwt', { session: false }),userController.updateUser);
-router.delete('/:id', passport.authenticate('jwt', { session: false }),userController.destroyUser);
+router.get('/:id', passport.authenticate('jwt', { session: false }), userController.showUser);
+router.put('/:id', passport.authenticate('jwt', { session: false }), userController.updateUser);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), userController.destroyUser);
 router.post('/login',userController.loginUser);
 
 module.exports = router;
