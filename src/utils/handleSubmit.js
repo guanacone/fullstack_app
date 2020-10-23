78bd196cc885fb6193e83f0018419e902928c68f
Gilles Rusca <gilles.rusca@gmail.com>
2020-10-23 10:39:17 -0600

    modified to accept token arg

--- src/utils/handleSubmit.js
+++ src/utils/handleSubmit.js
@@ -4,7 +4,12 @@ import { navigate } from 'gatsby';
 const handleSubmit = async ({ evt, method, endpoint, data }) => {
   evt.preventDefault();
   try {
-    const response = await Axios({ method, url: endpoint, data });
+    const userToken = JSON.parse(window.localStorage.getItem('gatsbyUser')) ? JSON.parse(window.localStorage.getItem('gatsbyUser')) : null;
+    const response = await Axios({
+      method,
+      url: endpoint,
+      data,
+      headers: { Authorization: `Bearer ${userToken}` } });
     navigate(`/user/${response.data._id}`);
   } catch (err) {
     console.log(err);
