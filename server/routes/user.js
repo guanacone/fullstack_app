b84f3cee6fa06cde068542d527cd4243c9677978
Gilles Rusca <gilles.rusca@gmail.com>
2020-11-03 08:04:39 -0600

    corrected error on asynchandler

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,11 +6,11 @@ const asyncHandler = require('express-async-handler');
 
 const passportAuth = passport.authenticate('jwt', { session: false });
 
-router.get('/', asyncHandler(passportAuth, userController.indexUser));
+router.get('/', passportAuth, asyncHandler(userController.indexUser));
 router.post('/', asyncHandler(userController.createUser));
-router.get('/:id', asyncHandler(passportAuth, userController.showUser));
-router.put('/:id', asyncHandler(passportAuth, userController.updateUser));
-router.delete('/:id', asyncHandler(passportAuth, userController.destroyUser));
+router.get('/:id', passportAuth, asyncHandler(userController.showUser));
+router.put('/:id', passportAuth, asyncHandler(userController.updateUser));
+router.delete('/:id', passportAuth, asyncHandler(userController.destroyUser));
 router.post('/login',asyncHandler(userController.loginUser));
 
 module.exports = router;
