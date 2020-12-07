55995c147c86a95a80ed0cf40b6d432562cb76ae
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-07 16:41:22 -0600

    modified redirect

--- src/utils/handleSubmit.js
+++ src/utils/handleSubmit.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
+import { navigate } from 'gatsby';
 import { getUser } from '../services/auth';
 
-const handleSubmit = async ({ evt, method, endpoint, data }) => {
+const handleSubmit = async ({ evt, method, endpoint, data, destination }) => {
   evt.preventDefault();
   try {
     const user = getUser();
@@ -9,7 +10,9 @@ const handleSubmit = async ({ evt, method, endpoint, data }) => {
       method,
       url: endpoint,
       data,
-      headers: { Authorization: `Bearer ${user.token}` } });
+      headers: { Authorization: `Bearer ${user.token}` },
+    });
+    navigate(destination);
   } catch (err) {
     const { response } = err;
     alert(response.data.message);
