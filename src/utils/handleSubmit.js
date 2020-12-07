ec40c303b90d562343c990e79b9c3c91f062eaae
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-07 16:33:28 -0600

    useer activation logic

--- src/utils/handleSubmit.js
+++ src/utils/handleSubmit.js
@@ -1,17 +1,15 @@
 import Axios from 'axios';
-import { navigate } from 'gatsby';
 import { getUser } from '../services/auth';
 
 const handleSubmit = async ({ evt, method, endpoint, data }) => {
   evt.preventDefault();
   try {
     const user = getUser();
-    const response = await Axios({
+    await Axios({
       method,
       url: endpoint,
       data,
       headers: { Authorization: `Bearer ${user.token}` } });
-    navigate(`/user/${response.data._id}`);
   } catch (err) {
     const { response } = err;
     alert(response.data.message);
