85e2957e79e95aad351bcab7087f2c438bfaf424
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-09 10:13:51 -0600

    promisifyed sendEmail util

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,10 +1,10 @@
 const mailgun = require('mailgun-js');
-const mgApiKey = '83953b8460b588a67a3c6e26a5945f22-95f6ca46-b77626b0';
-const domain = 'sandboxfc5e54316a7244c1b1b13fe82614124c.mailgun.org';
+const mgApiKey = process.env.MAILGUN_API_KEY;
+const domain = process.env.MAILGUN_DOMAIN;
 const mg = mailgun({ apiKey: mgApiKey, domain });
 
-exports.sendEmail = (data) => {
-  mg.messages().send(data, (error, body) => {
+exports.sendEmail = async (data) => {
+  await mg.messages().send(data, (error, body) => {
     console.log(body);
   });
 };
