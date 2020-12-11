5dcf5ebf82adaa6779ee4372a2b54c64964324d0
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-11 06:47:06 -0600

    corrected lint, logging GATSBY_ env var

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
 const { NODE_ENV } = process.env;
-const url = NODE_ENV === 'production' ? `https://${HEROKU_APP_NAME}.herokuapp.com/api` : 'http://localhost:1337/api';
-console.log('app name: ', process.env.HEROKU_APP_NAME, 'host: ', process.env.HOST);
+const url = NODE_ENV === 'production' ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api` : 'http://localhost:1337/api';
+console.log('app name: ', process.env.GATSBY_HEROKU_APP_NAME);
 export default url;
