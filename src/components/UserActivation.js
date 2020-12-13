a8880c96d33a1aa381cf64ea2822b8a3b2d4c14a
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-13 07:56:33 -0600

    user activation component

---
+++ src/components/UserActivation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserActivation = ({ token }) => {
+  console.log('user activation');
+  return (
+    <p>token is: {token}</p>
+  );
+};
+
+export default UserActivation;
