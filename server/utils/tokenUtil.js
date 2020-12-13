98229b682245c0428f72aca6cdc816deb843a009
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-13 08:07:19 -0600

    added func to extract token from header

--- server/utils/tokenUtil.js
+++ server/utils/tokenUtil.js
@@ -1,6 +1,14 @@
 const jwt = require('jsonwebtoken');
 
+exports.extractTokenFromHeader = (req) => {
+  const { authorization } = req.headers;
+  const bearer = authorization.split(' ');
+  const token = bearer[1];
+  return token;
+};
+
 exports.isTokenExpired = (req) => {
-  const { exp } = jwt.decode(req.query.token);
-  return ((exp * 1000) < Date.now()) ? true : null;
+  const token = this.extractTokenFromHeader(req);
+  const { exp } = jwt.decode(token);
+  return ((exp * 1e3) < Date.now()) ? true : null;
 };
