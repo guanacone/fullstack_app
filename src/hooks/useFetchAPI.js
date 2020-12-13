ef4f09732ac84a42c848fc2b478a1e2e187a49e8
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-13 10:49:53 -0600

    added token as arg

--- src/hooks/useFetchAPI.js
+++ src/hooks/useFetchAPI.js
@@ -1,18 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { getUser } from '../services/auth';
 
-const useFetchAPI = ({ endpoint }) => {
+const useFetchAPI = ({ endpoint, token }) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     (async () => {
       try {
-        const user = getUser();
+        // const user = getUser();
         const result = await axios({
           url: endpoint,
-          headers: { Authorization: `Bearer ${user.token}` },
+          headers: { Authorization: `Bearer ${token}` },
         });
         setData(result.data);
       } catch (err) {
