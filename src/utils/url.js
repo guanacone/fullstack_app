d050de57d55f632ef65bd60c6fad4bf8bcf75066
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-14 09:18:44 -0600

    logging

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,4 @@
 const { NODE_ENV } = process.env;
 const url = NODE_ENV === 'production' ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api` : 'http://localhost:1337/api';
 export default url;
+console.log('logging');
