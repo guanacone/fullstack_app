193a7fa4cd35ab44c66953d4a54f93ed20343e89
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-15 09:03:46 -0600

    get hostname

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,6 @@
+export const isBrowser = () => typeof window !== 'undefined';
+
 const { NODE_ENV } = process.env;
-const url = NODE_ENV === 'production' ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api` : 'http://localhost:1337/api';
+
+const url = (NODE_ENV === 'production') && isBrowser() ? window.location.host : 'http://localhost:1337/api';
 export default url;
