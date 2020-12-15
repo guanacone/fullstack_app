6a4b81032ffd7a85a304d8afdd5b1b7cdfbbaa90
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-15 07:51:00 -0600

    removed unused console.log

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,3 @@
 const { NODE_ENV } = process.env;
 const url = NODE_ENV === 'production' ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/api` : 'http://localhost:1337/api';
 export default url;
-console.log('logging');
