89b8c8783c560dd131f6455efbe954a70112bf34
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-15 10:05:54 -0600

    modified url

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,5 +2,5 @@ export const isBrowser = () => typeof window !== 'undefined';
 
 const { NODE_ENV } = process.env;
 
-const url = (NODE_ENV === 'production') && isBrowser() ? window.location.hostname : 'http://localhost:1337/api';
+const url = (NODE_ENV === 'production') && isBrowser() ? `${window.location.hostname}/api` : 'http://localhost:1337/api';
 export default url;
