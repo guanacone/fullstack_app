ba63056d665176250fb3993ccb77ce031af81dec
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-18 07:11:02 -0600

    account activation logic

--- src/components/UserActivation.js
+++ src/components/UserActivation.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link, navigate } from 'gatsby';
+import get from 'lodash.get';
+import { isLoggedIn } from '../services/auth';
+import useFetchAPI from '../hooks/useFetchAPI';
 
-const UserActivation = ({ token }) => {
-  console.log('user activation');
+const UserActivation = ({ activationToken }) => {
+  if (isLoggedIn()) {
+    navigate('/user');
+  }
+  const { data, error } = useFetchAPI({ endpoint: '/user/activate_account', token: activationToken });
+  const getContent = (dataContent, errorContent) => {
+    if (errorContent) {
+      const axiosMsg = get(errorContent, ['response', 'data', 'message']);
+      if (axiosMsg) {
+        return <p>Your token has expired. Please <Link to={'/user/new'}>sign up</Link> again</p>;
+      }
+      return (
+        <p>{errorContent.message}</p>
+      );
+    }
+
+    if (dataContent) {
+      return (
+        <>
+          <p>Your account has been activated. Please <Link to={'/login'}>log in</Link></p>
+        </>
+      );
+    }
+    return (
+      <p>loading...</p>
+    );
+  };
+
   return (
-    <p>token is: {token}</p>
+    <div>
+      <h1>Account activated</h1>
+      {getContent(data, error)}
+    </div>
   );
 };
 
