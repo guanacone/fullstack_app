1611314aa65ecd0741595380c0bce726098b736d
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-21 09:00:40 -0600

    replaced mailgun-js with mailgun.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,11 +1,11 @@
-const mailgun = require('mailgun-js');
+const mailgun = require('mailgun.js');
 const mgApiKey = process.env.MAILGUN_API_KEY;
-const domain = process.env.MAILGUN_DOMAIN;
-const mg = mailgun({ apiKey: mgApiKey, domain });
+const mgDomain = process.env.MAILGUN_DOMAIN;
+const mg = mailgun.client({ username: 'api', key: mgApiKey });
 
 exports.sendEmail = async (data) => {
   try {
-    const body = await mg.messages().send(data);
+    const body = await mg.messages.create(mgDomain, data);
     console.log(body);
   } catch (error) {
     console.log(error);
