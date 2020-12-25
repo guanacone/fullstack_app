84463e42c981a06a672e96eee9d73d696be61225
Gilles Rusca <gilles.rusca@gmail.com>
2020-12-25 07:48:34 -0600

    corrected type and folder path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const gatsyExpress = require('gatsby-plugin-express');
+const gatsbyExpress = require('gatsby-plugin-express');
 require('dotenv').config();
 
 const mongoDB = 'mongodb+srv://mogador:basket@cluster0.twty6.mongodb.net/fullstack_app?retryWrites=true&w=majority';
@@ -19,10 +19,10 @@ app.use(express.json());
 
 require('./auth');
 
-app.use(express.static('public'));
-app.use(gatsyExpress('config/gatsby-express.json', {
-  publicDir: 'public/',
-  template: 'public/404/index.html',
+app.use(express.static('../public'));
+app.use(gatsbyExpress('config/gatsby-express.json', {
+  publicDir: '../public/',
+  template: '../public/404/index.html',
 
   // redirects all /path/ to /path
   // should be used with gatsby-plugin-remove-trailing-slashes
