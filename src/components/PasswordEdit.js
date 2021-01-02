1210a6b15f0c66c810431e8e3aada6f0b4350a6f
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-02 11:24:33 -0600

    refactored handle submit

--- src/components/PasswordEdit.js
+++ src/components/PasswordEdit.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import { navigate } from 'gatsby';
 import useInput from '../hooks/useInput';
 import handleSubmit from '../utils/handleSubmit';
+import { getUser } from '../services/auth';
 
 const PasswordEdit = ({ location }) => {
   const userID = location.pathname.split('/')[2];
   const oldPassword = useInput('');
   const newPassword = useInput('');
   const confirmNewPassword = useInput('');
+  const user = getUser();
 
   return (
-    <form onSubmit={(evt) => {
-      evt.preventDefault();
-      if (newPassword.value !== confirmNewPassword.value) {
-        alert('new passwords do not match');
-      } else {
-        handleSubmit({
-          evt,
-          method: 'put',
-          endpoint: `/user/update_password/${userID}`,
-          data: {
-            oldPassword: oldPassword.value,
-            newPassword: newPassword.value,
-          },
-          destination: `/user/${userID}`,
-        });
-      }
-    }}>
+    <form onSubmit={
+      async (evt) => {
+        evt.preventDefault();
+        if (newPassword.value !== confirmNewPassword.value) {
+          alert('new passwords do not match');
+        } else {
+          try {
+            await handleSubmit({
+              method: 'put',
+              endpoint: `/user/update_password/${userID}`,
+              data: {
+                oldPassword: oldPassword.value,
+                newPassword: newPassword.value,
+              },
+              token: user.token,
+            });
+            navigate(`/user/${userID}`);
+          } catch (err) {
+            const { response } = err;
+            alert(response.data.message);
+          }
+        }
+      }}>
       <label>
         Old password:
         <input type='password' required {...oldPassword.bind}/>
