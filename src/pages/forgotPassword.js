d1fe344ffa9a646ac67dc2286518efc3b71d61f2
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-03 10:00:49 -0600

    refactored handleSubmit

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,23 +1,29 @@
+import { navigate } from '@reach/router';
 import React from 'react';
 import useInput from '../hooks/useInput';
 import handleSubmit from '../utils/handleSubmit';
 
 const ForgotPassword = () => {
   const userEmail = useInput('');
-  console.log('email: ', userEmail.value);
   return (
-    <form onSubmit={(evt) => {
-      evt.preventDefault();
-      handleSubmit({
-        evt,
-        method: 'post',
-        endpoint: '/user/send_reset_password_link',
-        data: {
-          email: userEmail.value,
-        },
-        destination: '/login',
-      });
-    }
+    <form onSubmit={
+      async (evt) => {
+        evt.preventDefault();
+        try {
+          await handleSubmit({
+            evt,
+            method: 'post',
+            endpoint: '/user/send_reset_password_link',
+            data: {
+              email: userEmail.value,
+            },
+          });
+          navigate('/login');
+        } catch (err) {
+          const { response } = err;
+          alert(response.data.message);
+        }
+      }
     }>
       <label>
         Your email:
