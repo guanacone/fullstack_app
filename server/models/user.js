df2d7bcd494508d2154c851a822b919c8bba3955
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-03 10:32:21 -0600

    added isAdmin fielt to user model

--- server/models/user.js
+++ server/models/user.js
@@ -9,6 +9,7 @@ const UserSchema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isActivated: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
     expireAt: { type: Date, default: null },
   },
 );
