39c741686ab113637ed3838601ac3b9a41806f2d
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-04 10:43:00 -0600

    replaced isAdmin with roles array

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isActivated: { type: Boolean, default: false },
-    isAdmin: { type: Boolean, default: false },
+    roles: { type: Array, default: [] },
     expireAt: { type: Date, default: null },
   },
 );
