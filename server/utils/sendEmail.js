f664d5dc838acf75b17c92bb9253a5bb7065e69a
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-28 10:26:56 -0600

    updated mailgun secrets

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,6 +1,6 @@
 const mailgun = require('mailgun-js');
-const mgApiKey = '83953b8460b588a67a3c6e26a5945f22-95f6ca46-b77626b0';
-const domain = 'sandboxfc5e54316a7244c1b1b13fe82614124c.mailgun.org';
+const mgApiKey = process.env.CONFIRMATION_TOKEN_SECRET;
+const domain = process.env.MAILGUN_DOMAIN;
 const mg = mailgun({ apiKey: mgApiKey, domain });
 
 exports.sendEmail = (data) => {
